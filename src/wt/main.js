63fdc47d83b7bb179b90492a2ed7fb91b9e6b260
Nikolay Kovzik <nikolay.kovzik@mail.ru>
2023-06-20 02:01:51 +0300

    feat: implement worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker } from "worker_threads";
+import os from "os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = join(__dirname, "worker.js");
+
+const DEFAULT_START_NUMBER = 10;
+
+const createNewWorker = (fibNumber) => {
+  return new Promise((resolve) => {
+    const worker = new Worker(workerPath, {
+      workerData: { fibNumber },
+    });
+
+    worker.on("message", (result) => {
+      resolve({ status: "resolved", data: result });
+    });
+
+    worker.on("error", () => {
+      resolve({ status: "error", data: null });
+    });
+  });
 };
 
-await performCalculations();
+const performCalculations = async () => {
+  const cpuCores = os.cpus().length;
+  const workersPool = [];
+
+  for (let i = 0; i < cpuCores; i++) {
+    const fibNumber = DEFAULT_START_NUMBER + i;
+    const workerPromise = createNewWorker(fibNumber);
+    workersPool.push(workerPromise);
+  }
+
+  const results = await Promise.all(workersPool);
+  console.log(results);
+};
+
+performCalculations();
