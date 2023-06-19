1dbf8dc329f24b115a25bf581af28704199a604d
Nikolay Kovzik <nikolay.kovzik@mail.ru>
2023-06-20 02:31:22 +0300

    feat: implement child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptFileName = join(__dirname, 'files', 'script.js');
+    
+    const child = fork(scriptFileName, args, { silent: true });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
